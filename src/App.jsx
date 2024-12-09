126b645b746cb21859ccb84660a80e7713231e36
Siddhesh Chavan <149790685+siddheshchavan2211@users.noreply.github.com>
2024-12-09 13:43:02 +0000

    vite+react+tailwind config done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1 className="bg-red-400 text-6xl sm:bg-amber-400 md:bg-slate-400 lg:bg-green-400">
+        {" "}
+        hello
+      </h1>
+    </>
+  );
+}
+
+export default App;
