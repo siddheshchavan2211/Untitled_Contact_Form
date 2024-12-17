305965f24f8cb9d53ce1b68425596a9ae1748f37
Siddhesh Chavan <149790685+siddheshchavan2211@users.noreply.github.com>
2024-12-17 14:13:37 +0000

    validations

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -34,23 +34,40 @@ const Form = () => {
           id="fullname"
           placeholder="Your name"
           className="border-b border-stone-700 p-2 placeholder-gray-700 md:bg-sky-300"
-          {...register("fullname")}
+          {...register("fullname", { required: "please enter your name" })}
         />
+        {errors.fullname && (
+          <p className="text-red-500">{errors.fullname.message}</p>
+        )}
         <input
           type="email"
           id="email"
           placeholder="you@company.com"
           className="border-b border-stone-700 p-2 placeholder-gray-700 md:bg-sky-300"
-          {...register("email")}
+          {...register("email", { required: "please enter your email",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: "invalid email",
+            },
+           })}
         />
+        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
         <input
           type="text"
           id="message"
           placeholder="Tell us a bit about your project..."
           className="h-24 border-b border-stone-700 p-2 placeholder-gray-700 md:bg-sky-300"
-          {...register("message")}
+          {...register("message", {
+            required: "please enter your message",
+            minLength: {
+              value: 10,
+              message: "Please enter at least 10 characters",
+            },
+          })}
         />
-
+        {errors.message && (
+          <p className="text-red-500">{errors.message.message}</p>
+        )}
         <p className="my-5 text-gray-800">How can we help?</p>
 
         <div className="mb-5 grid max-w-96 grid-cols-2">
@@ -62,14 +79,18 @@ const Form = () => {
                   id=""
                   className="size-5"
                   value={service}
-                  {...register("services")}
+                  {...register("services", {
+                    required: "please select atleast one service",
+                  })}
                 />
                 {service}
               </label>
             );
           })}
         </div>
-
+        {errors.services && (
+          <p className="text-red-500">{errors.services.message}</p>
+        )}
         <button
           type="submit"
           className="flex items-center justify-center gap-2 rounded bg-zinc-950 p-2 text-white"
