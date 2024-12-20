b07753c20fc8e7f696c40c5876faf462a7dd1561
Siddhesh Chavan <149790685+siddheshchavan2211@users.noreply.github.com>
2024-12-20 12:43:59 +0000

    chages done

--- src/pages/Submission.jsx
+++ src/pages/Submission.jsx
@@ -12,11 +12,9 @@ const Submission = () => {
       <Header />
 
       <div className="rounded-lg bg-lime-400 p-5 text-4xl text-white">
-        <h1>Oops...</h1>
         <h1>Form Submission Successfull 👍🏻</h1>
         <h4>thank: {name}</h4>
         <p>
-          Check here{" "}
           <a target="_blank" href={config.userResponse} className="underline">
             click
           </a>
